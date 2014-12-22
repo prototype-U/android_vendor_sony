47086be6f090713286efb22a37a47f93cf18733b
Naman Arora <naman.arora11@gmail.com>
2014-12-22 11:00:16 +0530

    source

---
+++ aries/aries-vendor.mk
@@ -0,0 +1,3 @@
+PRODUCT_PLATFORM := shinano
+$(call inherit-product, $(LOCAL_PATH)/aries-partial.mk)
+$(call inherit-product, vendor/sony/shinano/shinano-partial.mk)
---
+++ castor/castor-vendor.mk
@@ -0,0 +1,3 @@
+PRODUCT_PLATFORM := shinano
+$(call inherit-product, $(LOCAL_PATH)/castor-partial.mk)
+$(call inherit-product, vendor/sony/shinano/shinano-partial.mk)
